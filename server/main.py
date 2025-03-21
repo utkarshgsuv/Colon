fa5d9a76941715da11fd3f67cfca0d46928ecfa8
Utkarsh Maheshwari <utkarshgsuv@gmail.com>
2025-03-21 13:00:12 +0530

    added python

--- server/main.py
+++ server/main.py
@@ -38,4 +38,9 @@ def colon_endpoint(body: ChatBody):
         print(e)
     
     
+    
+if __name__ == "__main__":
+    import uvicorn
+    PORT = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
 
